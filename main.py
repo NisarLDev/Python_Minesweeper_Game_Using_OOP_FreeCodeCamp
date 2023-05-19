c3a7538872a65aba8054fd771c4bb61487977cbc
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-19 04:32:10 +0200

    Top_frame

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ root.geometry('1440x720')
 root.title("Minesweeper Game")
 root.resizable(False, False)
 
-top_frame = Frame(root, bg='red', # Change later to black, width=1400,height=180)
+top_frame = Frame(root, bg='red', 
+    # Change later to black, 
+    width=1400,
+    height=180)
 top_frame.place()
 
 # Run the Window
