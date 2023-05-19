13fcd486695515baf878036a6c2755d57b660731
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-19 06:38:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,17 +16,9 @@ left_frame = Frame(root, bg='black', width=utils.width_prct(25), height=utils.he
 
 let_flame.place(x=0, y=utils.height_prct(25))
                   
-center_frame= Frame(
-    root,
-    bg='black',
-    width=utils.width_prct(75)
-    height=utils.height_prct(75)
-)
+center_frame= Frame(root, bg='black', width=utils.width_prct(75), height=utils.height_prct(75))
                   
-center_frame.place(
-    x=utils.width_prct(25),
-    y=utils.height_prct(25),
-)
+center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
 
 for x in range(settings.GRID_SIZE):
     for y in range(settings.GRID_SIZE):
