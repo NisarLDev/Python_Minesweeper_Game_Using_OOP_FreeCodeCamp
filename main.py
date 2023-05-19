2c7503b5095356547720968b34f641cbe83b2a62
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-19 04:38:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,5 +14,12 @@ top_frame = Frame(root, bg='red',
     height=180)
 top_frame.place(x=0, y=0)
 
+left_frame=Frame(
+    root,
+    bg='blue',# Change later to black
+    width=360,
+    height=540
+)
+
 # Run the Window
 root.mainloop()
