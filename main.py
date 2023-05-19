c96bfb1460af60a09fb5444d12bbf76f43e4f58f
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-19 06:37:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,8 @@ root.resizable(False, False)
 
 top_frame = Frame(root, bg='black', width=settings.WIDTH, height=utils.height_pctr(25), top_frame.place(x=0, y=0)
 
-left_frame = Frame(
-    root,
-    bg='black',
-    width=utils.width_prct(25),
-    height=utils.height_prct(75)
-)
+left_frame = Frame(root, bg='black', width=utils.width_prct(25), height=utils.height_prct(75))
+
 let_flame.place(x=0, y=utils.height_prct(25))
                   
 center_frame= Frame(
