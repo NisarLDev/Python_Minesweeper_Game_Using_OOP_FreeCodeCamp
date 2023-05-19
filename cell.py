3024ac0c8472d532b295240f2cdd719674e6f790
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-19 07:55:19 +0200

    Update cell.py

--- cell.py
+++ cell.py
@@ -53,6 +53,14 @@ class Cell:
         cells = [cell for cell in surrounded_cells if cell is not None]
         return cells
     
+    @property
+    def surrounded_cells_mines_length(self):
+        counter = 0
+        for cell in self.surrounded_cells:
+            if cell.is_mine:
+                counter += 1
+        return counter
+    
     def show_cell(self):
         print(self.surrounded_cells)
         
