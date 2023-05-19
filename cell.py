41dfbb90b2ca3f274154d51aa2c95347e78bb1a8
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-19 05:55:35 +0200

    Create Button Right Click

--- cell.py
+++ cell.py
@@ -9,7 +9,9 @@ class Cell:
             text='Text'
         )
         self.cell_btn_object = btn
-        btn.bind('<Button-1>', self.left_click_actions )
+        btn.bind('<Button-1>', self.left_click_actions ) # Left Click
+        btn.bind('<Button-3>', self.right_click_actions ) # Right Click
+
         self.cell_btn_object = btn
 
     defleft_click_actions(self, event):
