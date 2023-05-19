a5144c907824b56bd6dbcd19af3b485784dd04d6
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-19 09:06:13 +0200

    Update cell.py

--- cell.py
+++ cell.py
@@ -97,7 +97,7 @@ class Cell:
         #A logic do interrupt thegame anddisplay a message that player lost!
         self.cell_btn_object.configure(bg='red')
     
-    def right_click_actions(selft, event):
+    def right_click_actions(self, event):
         if not self.is_mine_candidate:
             self.cell_btn_object.configure(
                 bg='orange'
