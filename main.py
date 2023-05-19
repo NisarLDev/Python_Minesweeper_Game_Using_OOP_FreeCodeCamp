51eb1024c4556f65a402b59fb121a2cfa5108c01
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-19 05:01:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,5 +23,13 @@ left_frame = Frame(
     height=utils.height_prct(75)
 )
 let_flame.place(x=0, y=utils.height_prct(25))
+                  
+center_frame= Frame(
+    root,
+    bg='green', # Change laterto black
+    width=utils.width_prct(75)
+    height=utils.height_prct(75)
+)
+                  
 # Run the Window
 root.mainloop()
