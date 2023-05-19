8cc73e85991a351a1ac643e0477d4b7796678587
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-19 04:55:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ top_frame.place(x=0, y=0)
 left_frame = Frame(
     root,
     bg='blue',# Change later to black
-    width=360,
+    width=utils.width_prct(25),
     height=540
 )
 let_flame.place(x=0, y=180)
